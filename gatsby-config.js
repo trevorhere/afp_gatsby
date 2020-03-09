2f62bd989c1841e47fed5a4199bdd3b2b0317340
Trevor Lane <tlane@simplenexus.com>
2020-03-08 22:06:50 -0600

    fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,15 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          `gatsby-remark-video-poster`,
-        ],
-      },
     },
+    {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        `gatsby-remark-video-poster`,
+      ],
+    },
+  },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
